82fbf455395154c9c0538ad0440cdac6342adc61
Kristiyan Todorov <thedivkiller@gmail.com>
2024-08-14 20:24:00 +0300

    displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
 import { CanceledError } from 'axios';
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -24,9 +31,8 @@ const useGame = () => {
             .get<FetchGamesResponse>('/games', { signal: controller.signal })
             .then((res) => setGames(res.data.results))
             .catch((error) => {
-                if ( error instanceof CanceledError ) return;
+                if (error instanceof CanceledError) return;
                 setError(error.message);
-                
             });
 
         return () => controller.abort();
