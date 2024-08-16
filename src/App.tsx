df76030ad8c142859933b6e7762bf5638f35f7ed
Kristiyan Todorov <thedivkiller@gmail.com>
2024-08-16 15:45:03 +0300

    heighlight the selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,13 @@ const App = () => {
             <Show above="lg">
                 <GridItem area="aside" paddingX={5}>
                     <GenreList
+                        selectedGenre={selectedGenre}
                         onSelectGenre={(genre) => setSelectedGenre(genre)}
                     />
                 </GridItem>
             </Show>
             <GridItem area="main">
-                <GameGrid selectedGenre={selectedGenre}/>
+                <GameGrid selectedGenre={selectedGenre} />
             </GridItem>
         </Grid>
     );
